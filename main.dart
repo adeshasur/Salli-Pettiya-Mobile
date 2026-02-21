f33bfadcae54847be1d753d4d3e3f23c3ba98db3
ADHEESHA SOORIYAARACHCHI <106448404+adeshasur@users.noreply.github.com>
2026-02-21 11:41:29 +0530

    Added AppColors and configured Dark Theme in main.dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'theme/app_colors.dart';
+import 'screens/dashboard_screen.dart';
+
+void main() {
+  runApp(const SalliPettiyaApp());
+}
+
+class SalliPettiyaApp extends StatelessWidget {
+  const SalliPettiyaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Salli Pettiya',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.pureBlack,
+      ),
+      home: const DashboardScreen(),
+    );
+  }
+}
---
+++ theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color neonGreen = Color(0xFF39FF14);
+  static const Color pureBlack = Color(0xFF000000);
+  static const Color darkCard = Color(0xFF111111);
+  static const Color spotifyGreen = Color(0xFF1DB954);
+}
